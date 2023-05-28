f3b0f3cf07618890f14aa1cfa2a1ade57b2f06a5
Ayush Jaipuriyar <ayushjaipuriyar21@gmail.com>
2023-05-28 15:38:16 +0530

    README

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
-# covin-assessment
+# Django REST API for Google Calendar Integration
+This repository contains a Django REST API for integrating with Google Calendar. The API provides two endpoints for initializing the Google authentication process and handling the authentication redirect.
+
+## Endpoints
+/rest/v1/calendar/init/: Initializes the Google authentication and provides the authentication URL for OAuth2. Once authenticated, it redirects to the next endpoint /rest/v1/calendar/redirect/.
+
+/rest/v1/calendar/redirect/: Handles the authentication redirect from Google. Retrieves the user's credentials and uses them to access the Google Calendar API. Returns a response containing the user's calendar events.
+
+## Prerequisites
+Before running the API, make sure you have the following prerequisites:
+```
+Django (version X.X.X)
+Django REST Framework (version X.X.X)
+Google API client library
+```
+## Installation
+
+1. Clone the repository:
+
+bash
+git clone https://github.com/your-username/your-repository.git
+cd your-repository
+
+Install the dependencies:
+
+bash
+Copy code
+pip install -r requirements.txt
+Configure Google OAuth2:
+
+Obtain the client secret JSON file from the Google Developer Console.
+Rename the client secret JSON file to google_oauth_client_secret.json.
+Replace settings.GOOGLE_OAUTH_CLIENT_SECRET in GoogleCalendarInitView and GoogleCalendarRedirectView with the path to your google_oauth_client_secret.json file.
+Apply database migrations:
+
+bash
+Copy code
+python manage.py migrate
+Start the development server:
+
+bash
+Copy code
+python manage.py runserver
+The API will be accessible at http://localhost:8000/.
+
+Usage
+You can interact with the API using your preferred REST client (e.g., cURL, Postman). Here's an example workflow:
+
+Initialize Google authentication by making a GET request to /rest/v1/calendar/init/. This endpoint will return a response containing the authorization URL.
+
+Redirect the user to the obtained authorization URL. After authentication, the user will be redirected to /rest/v1/calendar/redirect/.
+
+Make a GET request to /rest/v1/calendar/redirect/ to retrieve the user's calendar events. The API will handle the authentication and return a response containing the calendar events.
+
+Code Explanation
+The code contains two functions, GoogleCalendarInitView and GoogleCalendarRedirectView, which correspond to the two API endpoints.
+
+GoogleCalendarInitView: Initializes the Google authentication process. If the user's credentials are already stored in the session, it redirects to the authentication URL. Otherwise, it generates the authorization URL and stores the state parameter in the session.
+
+GoogleCalendarRedirectView: Handles the authentication redirect from Google. It retrieves the user's credentials, validates them, and uses them to access the Google Calendar API. It then retrieves the user's calendar events and returns them in the response.
+
+Make sure to update the code according to your requirements and configure the necessary settings.
+
--- calendar_integration/calendar_api/views.py
+++ calendar_integration/calendar_api/views.py
@@ -1,35 +1,113 @@
-from django.shortcuts import redirect
+# from django.shortcuts import redirect
 from django.conf import settings
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
+# from google.oauth2.credentials import Credentials
+# from google_auth_oauthlib.flow import Flow
+
+# from googleapiclient.discovery import build
+from django.urls import reverse
+# import os
+# import json
+
+
+# os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+# SCOPE = ['https://www.googleapis.com/auth/calendar.readonly']
+# REDIRECT_URL = 'https://localhost:8000/rest/v1/calendar/redirect/'
+
+# def home(request):
+#     login_url = reverse('google_calendar_init')  # Assuming you have a URL pattern named 'google-login' for Google login
+#     return redirect(login_url)
+
+# @api_view(['GET'])
+# def GoogleCalendarInitView(request):
+#     if (request.session.get('credentials')):
+#         print(request.session.get('credentials'))
+#         return redirect(REDIRECT_URL)
+#     flow = Flow.from_client_secrets_file(
+#         settings.GOOGLE_OAUTH_CLIENT_SECRET,
+#         scopes=SCOPE,
+#         redirect_uri=REDIRECT_URL
+#     )
+#     authorization_url, state = flow.authorization_url(
+#         access_type='offline',
+#     )
+#     request.session['state'] = state
+#     return Response({"authorization_url": authorization_url})
+
+
+# @api_view(['GET'])
+# def GoogleCalendarRedirectView(request):
+#     credentialsSession = request.session.get('session')
+#     if credentialsSession is not None:
+#         credentials = Credentials.from_authorized_user_info(
+#                 json.loads(credentialsSession))
+#     else:
+#         credentials = Credentials(None)
+#     print("VALIDITY: ",credentials.valid)
+#     if not credentials or not credentials.valid:
+#         if credentials and credentials.expired and credentials.refresh_token:
+#             credentials.refresh(Request())
+#     else:
+#         state = request.session['state']
+#         if state is None:
+#             return Response({"error": "State parameter missing."})
+#         flow = Flow.from_client_secrets_file(
+#             settings.GOOGLE_OAUTH_CLIENT_SECRET,
+#             scopes=SCOPE,
+#             state=state,
+#             redirect_uri=REDIRECT_URL
+#         )
+#         authorization_response = request.get_full_path()
+#         print(authorization_response)
+#         flow.fetch_token(authorization_response=authorization_response)
+#         credentials = flow.credentials
+#         request.session['credentials'] = credentials.to_json()
+#         if 'credentials' not in request.session:
+#             return redirect('v1/calendar/init')
+#     service = build(
+#         'calendar', 'v3', credentials=credentials)
+#     calendar_list = service.calendarList().list().execute()
+#     calendar_id_list = [(item['id'], item['summary'])
+#                         for item in calendar_list['items']]
+#     events_list = {}
+#     for x in calendar_id_list:
+#         events = service.events().list(
+#             calendarId=x[0], maxResults=10).execute()
+#         events_list.update({x[1]: events['items']})
+#     return Response(events_list)
+
+
+from django.shortcuts import redirect
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import Flow
-from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
-from django.urls import reverse
 import os
 import json
-
-
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
-SCOPE = ['https://www.googleapis.com/auth/calendar.readonly']
+SCOPES = ['https://www.googleapis.com/auth/calendar']
 REDIRECT_URL = 'https://localhost:8000/rest/v1/calendar/redirect/'
 
+
 def home(request):
-    login_url = reverse('google_calendar_init')  # Assuming you have a URL pattern named 'google-login' for Google login
+    login_url = reverse('google_calendar_init')  
     return redirect(login_url)
 
 @api_view(['GET'])
 def GoogleCalendarInitView(request):
-    if (request.session['credentials']):
-        print(request.session['credentials'])
+    if (request.session.get('credentials')):
+        print(request.session.get('credentials'))
         return redirect(REDIRECT_URL)
     flow = Flow.from_client_secrets_file(
         settings.GOOGLE_OAUTH_CLIENT_SECRET,
-        scopes=SCOPE,
-        redirect_uri=REDIRECT_URL
-    )
+        scopes=SCOPES,
+        redirect_uri=REDIRECT_URL)
     authorization_url, state = flow.authorization_url(
         access_type='offline',
     )
@@ -39,33 +117,50 @@ def GoogleCalendarInitView(request):
 
 @api_view(['GET'])
 def GoogleCalendarRedirectView(request):
-    credentials = Credentials.from_authorized_user_info(
-                json.loads(request.session['credentials']))
-    print("VALIDITY: ",credentials.valid)
-    if not credentials or not credentials.valid:
-        if credentials and credentials.expired and credentials.refresh_token:
-            credentials.refresh(Request())
+    if 'credentials' in request.session:
+        sessionCredentials = Credentials(**(request.session['credentials']))
+        if sessionCredentials.valid:
+            if sessionCredentials and sessionCredentials.expired and sessionCredentials.refresh_token:
+                sessionCredentials.refresh(Request())
+        else:
+            state = request.session['state']
+            flow = Flow.from_client_secrets_file(
+                settings.GOOGLE_OAUTH_CLIENT_SECRET, scopes=SCOPES,
+                state=state,
+                redirect_uri=REDIRECT_URL)
+            authorization_response = request.get_full_path()
+            flow.fetch_token(authorization_response=authorization_response)
+            credentials = flow.credentials
+            request.session['credentials'] = credentials_to_dict(credentials)
+            # if 'credentials' not in request.session:
+            #     return redirect('v1/calendar/init')
+            sessionCredentials = Credentials(**(request.session['credentials']))
+            service = build(
+                'calendar', 'v3', credentials=sessionCredentials)
+            calendar_list = service.calendarList().list().execute()
+            calendar_id_list = [(item['id'], item['summary'])
+                                for item in calendar_list['items']]
+            events_list = {}
+            for x in calendar_id_list:
+                events = service.events().list(
+                    calendarId=x[0], maxResults=10).execute()
+                events_list.update({x[1]: events['items']})
+            return Response(events_list)
     else:
         state = request.session['state']
-        if state is None:
-            return Response({"error": "State parameter missing."})
         flow = Flow.from_client_secrets_file(
-            settings.GOOGLE_OAUTH_CLIENT_SECRET,
-            scopes=SCOPE,
+            settings.GOOGLE_OAUTH_CLIENT_SECRET, scopes=SCOPES,
             state=state,
-            redirect_uri=REDIRECT_URL
-        )
+            redirect_uri=REDIRECT_URL)
         authorization_response = request.get_full_path()
-        print(authorization_response)
         flow.fetch_token(authorization_response=authorization_response)
         credentials = flow.credentials
-        request.session['credentials'] = credentials.to_json()
-        if 'credentials' not in request.session:
-            return redirect('v1/calendar/init')
-    credentials = Credentials.from_authorized_user_info(
-                json.loads(request.session['credentials']))
+        request.session['credentials'] = credentials_to_dict(credentials)
+        # if 'credentials' not in request.session:
+        #     return redirect('v1/calendar/init')
+    sessionCredentials = Credentials(**(request.session['credentials']))
     service = build(
-        'calendar', 'v3', credentials=credentials)
+        'calendar', 'v3', credentials=sessionCredentials)
     calendar_list = service.calendarList().list().execute()
     calendar_id_list = [(item['id'], item['summary'])
                         for item in calendar_list['items']]
@@ -75,3 +170,12 @@ def GoogleCalendarRedirectView(request):
             calendarId=x[0], maxResults=10).execute()
         events_list.update({x[1]: events['items']})
     return Response(events_list)
+
+
+def credentials_to_dict(credentials):
+    return {'token': credentials.token,
+            'refresh_token': credentials.refresh_token,
+            'token_uri': credentials.token_uri,
+            'client_id': credentials.client_id,
+            'client_secret': credentials.client_secret,
+            'scopes': credentials.scopes}
